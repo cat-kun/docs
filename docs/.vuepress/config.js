56192bc957959fa3ef6c1fe5da85781c7a7206b8
臭弟弟 <626269256@qq.com>
2022-04-24 17:31:13 +0800

    change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,27 +2,44 @@ const router = require('./public/routers/index')
 
 module.exports = {
   // 站点配置
-  // locales: {
-  //   lang: 'zh-CN',
-  // },
+  locales: {
+    // lang: 'zh-CN',
+    base: '/<vuePress>/',
+  },
   lang: 'zh-CN',
   title: '技术文档',
-  description: '这是我的第一个 VuePress 站点',  
+  description: '这是我的第一个 VuePress 站点',
   // 主题和它的配置
   theme: '@vuepress/theme-default',
   themeConfig: {
     logo: 'https://v2.vuepress.vuejs.org/images/hero.png',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: '青龙', link: '/qinglong/' },
-      // { text: 'External', link: 'https://google.com' },
+    navbar: [
+      { text: '首页', link: '/' },
+      {
+        text: '青龙相关',
+        children: [
+          {
+            text: '青龙部署',
+            link: '/qinglong/README.md'
+          },
+          {
+            text: 'Nark部署',
+            link: '/qinglong/Nark'
+          }
+        ],
+      },
+      { text: 'JD登录地址', link: 'http://ck.dramarie.com/' },
     ],
     sidebar: router,
     contributors: false,
     // contributorsText: 'Contributors'
   },
   markdown: {
-    plugins: ['task-lists']
+    plugins: [
+      'task-lists',
+    ]
   },
-  base: '/<vuePress>/'
+  plugins: [
+    '@vuepress/plugin-search',
+  ]
 }
--- docs/qinglong/README.md
+++ docs/qinglong/README.md
@@ -1,6 +1,6 @@
-## 青龙
+## 青龙🐉
 
-### 【新手推荐】青龙稳定版一键配置（QL pannel Faker Repository environment Setup）
+### 【新手推荐】青龙稳定版一键配置（QL pannel Faker Repository environment Setup）[参考链接](https://thin-hill-428.notion.site/QL-pannel-Faker-Repository-environment-Setup-45edcbfe90d74d8abb2d71896eab3be7)
 感谢@OreoMeow 大佬贡献配置
 
 #### 最新版青龙有可能造成脚本无法运行的情况，请使用一键脚本安装亲测稳定青龙版本。
