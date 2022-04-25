59a1fca80e5ac443c836bddc54fa5ce5dc19bb32
臭弟弟 <626269256@qq.com>
2022-04-25 11:53:16 +0800

    change

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,9 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:cat-kun/cat-kun.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cat-kun/vuePress.git master:gh-pages
+# git push -f git@github.com:cat-kun/vuePress.git master:gh-pages
 
 cd -
